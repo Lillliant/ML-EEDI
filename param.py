2460f28e93b9759d4a6ae90e4a7f4bfb4adb08e3
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-04-01 13:32:11 -0400

    Update param.py

--- param.py
+++ param.py
@@ -10,19 +10,6 @@ param_grid = {
         'objective': ['binary:logistic'],
         'max_depth': [1, 3, 5, 7, 10],
     },
-    'LR' : {
-        'C': [0.001, 0.01, 0.1, 1, 10, 100],
-        'penalty': ['l1', 'l2'],
-        'solver': ['sag', 'saga', 'liblinear'],
-        'max_iter': [10000],
-    },
-    'NN' : {
-        'hidden_layer_sizes': [(100,), (300,), (500,)],
-        'activation': ['relu', 'tanh'],
-        'solver': ['adam'],
-        'alpha': [0.01, 0.1, 1, 10],
-        'max_iter': [10000],
-    },
     'BASE' : {
         'strategy': ['stratified'],
     }
