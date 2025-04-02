7e1f75187ce5149c0ebdb5a650f74ec5cef6fdf7
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2025-04-01 22:27:34 -0400

    Add visualization

--- data/visualize.py
+++ data/visualize.py
@@ -6,14 +6,27 @@ import os
 
 # Create a histogram of the features with the data
 def plot_feature_histograms(data):
-    for column in data.columns:
-        plt.hist(data[column], bins=30, alpha=0.5, label=column)
-        plt.xlabel(column)
-        plt.ylabel('Frequency')
-        plt.title(f'Histogram of {column}')
-        plt.legend()
-        plt.show()
+    data = data.drop_duplicates()
+    features = ['UserId', 'QuestionId', 'Confidence']
+    print(features)
+    correct = data[data['IsCorrect'] == 1]
+    incorrect = data[data['IsCorrect'] == 0]
+    for key in features:
+        _, bins = np.histogram(data[key], bins=25)
+        plt.hist(correct[key], bins=bins, alpha=0.5, label="correct")
+        plt.hist(incorrect[key], bins=bins, alpha=0.5, label="incorrect")
+        plt.xlabel("Feature magnitude")
+        plt.ylabel("Frequency")
+        plt.title(f"Distribution for {key}")
+        plt.legend(loc="best")
+        plt.savefig(f'./{key}.png')
 
 # Create a scatter plot of the features with the data
 def plot_feature_scatter(data):
     pass
+
+if __name__ == "__main__":
+    data_path = './eedi/data.pkl'
+    if os.path.exists(data_path):
+        data = pd.read_pickle(data_path)
+        plot_feature_histograms(data)
